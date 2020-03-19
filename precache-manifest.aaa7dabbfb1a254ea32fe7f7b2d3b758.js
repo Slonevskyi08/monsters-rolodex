cf3b34b58b9c329d024b769fac7e291bdd253fcd
Mykhailo Slonevskyi <mykhailo.slonevskyi@gmail.com>
2020-03-19 15:32:06 +0200

    Updates

---
+++ precache-manifest.aaa7dabbfb1a254ea32fe7f7b2d3b758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b12a15936b49312036786292d5c42742",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "512052460b04d7d8b917",
+    "url": "/monsters-rolodex/static/css/main.c2558208.chunk.css"
+  },
+  {
+    "revision": "bc52861e46374f625869",
+    "url": "/monsters-rolodex/static/js/2.6debeacc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.6debeacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512052460b04d7d8b917",
+    "url": "/monsters-rolodex/static/js/main.f13090ad.chunk.js"
+  },
+  {
+    "revision": "26e484492ec11569b518",
+    "url": "/monsters-rolodex/static/js/runtime-main.4db7db6e.js"
+  }
+]);
